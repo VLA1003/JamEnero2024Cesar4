9c1c790dc97ce49e202fd6c4474469f5476a7ca1
DielopAlf <114227973+DielopAlf@users.noreply.github.com>
2024-01-29 12:39:14 +0000

    timer

--- Assets/_PROYECTO/SCRIPTS/timer.cs
+++ Assets/_PROYECTO/SCRIPTS/timer.cs
@@ -5,33 +5,49 @@ using UnityEngine.UI;
 
 public class timer : MonoBehaviour
 {
+    public static timer instance;
     public Slider timerSlider;
     public Text timerText;
-    public float gameTime;
+    [SerializeField] private float gameTime;
+
+    private float gameTime_;
 
     public bool stopTimer;
 
+    private void Awake()
+    {
+        if(instance != null && instance != this) 
+        {
+            Destroy(instance);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
         stopTimer = false;
         timerSlider.maxValue = gameTime;
         timerSlider.value = gameTime;
-
+        gameTime_ = gameTime;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        float time = gameTime - Time.time;
+        /*float time = gameTime - Time.time;
 
         int minutes = Mathf.FloorToInt(time / 60);
         int seconds = Mathf.FloorToInt(time -minutes* 60f);
 
-        string textTime = string.Format("{0:0}:{1:00}", minutes,seconds);
+        string textTime = string.Format("{0:0}:{1:00}", minutes,seconds);*/
 
-        if( time <= 0)
+        gameTime -= Time.deltaTime;
+
+        if( gameTime <= 0)
         {
 
             stopTimer = true;
@@ -40,11 +56,13 @@ public class timer : MonoBehaviour
         if(stopTimer == false)
         {
             //timerText.text = textTime;
-            timerSlider.value = time;
-
-
+            timerSlider.value = gameTime;
         }
+    }
 
-
+    public void ReinicioTiempo()
+    {
+        timerSlider.value = gameTime_;
+        gameTime = gameTime_;
     }
 }
