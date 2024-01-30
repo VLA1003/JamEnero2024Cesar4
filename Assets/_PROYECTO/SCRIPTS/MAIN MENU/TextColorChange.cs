5fded540a7cba7ca7d98cac810222481bdf26045
VLA1003 <114227795+VLA1003@users.noreply.github.com>
2024-01-30 17:14:29 +0000

    Arreglos en créditos - FINAL

--- Assets/_PROYECTO/SCRIPTS/MAIN MENU/TextColorChange.cs
+++ Assets/_PROYECTO/SCRIPTS/MAIN MENU/TextColorChange.cs
@@ -9,6 +9,8 @@ public class TextColorChange : MonoBehaviour
 {
     public TextMeshProUGUI playText;
     public TextMeshProUGUI exitText;
+    public TextMeshProUGUI creditText;
+    public TextMeshProUGUI backText;
 
     public void PlayTextWhite()
     {
@@ -28,5 +30,25 @@ public class TextColorChange : MonoBehaviour
     public void ExitTextBlack()
     {
         exitText.color = Color.black;
+    }
+
+    public void CreditTextWhite()
+    {
+        creditText.color = Color.white;
+    }
+
+    public void CreditTextBlack()
+    {
+        creditText.color = Color.black;
+    }
+
+    public void BackTextWhite()
+    {
+        backText.color = Color.white;
+    }
+
+    public void BackTextBlack()
+    {
+        backText.color = Color.black;
     }
 }
